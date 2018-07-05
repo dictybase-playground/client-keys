7ea1e660a8832b22ea9ba71c16f4f1f1a73804db
Eric Hartline <crossthebreeze@gmail.com>
2018-07-05 14:12:06 -0500

    add google key

--- clientConfig.js
+++ clientConfig.js
@@ -1,6 +1,7 @@
 const clientConfig = {
   orcid: { clientId: "APP-IHQJEZNOYXHHNZBM" },
   linkedin: { clientId: "77e54p8xh4n77e" },
+  google: { clientId: "660971614503-m99u7c1ro8u3uen5jft26l5qbnd078d7.apps.googleusercontent.com" }
 }
 
 export default clientConfig
