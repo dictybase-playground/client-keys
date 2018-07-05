1fce0dbc6198c04b97a3ef9e8944d6ff8d11e74f
Eric Hartline <crossthebreeze@gmail.com>
2018-07-05 14:50:43 -0500

    add fake facebook id

--- clientConfig.js
+++ clientConfig.js
@@ -1,7 +1,8 @@
 const clientConfig = {
   orcid: { clientId: "APP-IHQJEZNOYXHHNZBM" },
   linkedin: { clientId: "77e54p8xh4n77e" },
-  google: { clientId: "660971614503-m99u7c1ro8u3uen5jft26l5qbnd078d7.apps.googleusercontent.com" }
+  google: { clientId: "660971614503-m99u7c1ro8u3uen5jft26l5qbnd078d7.apps.googleusercontent.com" },
+  facebook: { clientId: "xxxxxxxxxxxxxxxxxx" }
 }
 
 export default clientConfig
