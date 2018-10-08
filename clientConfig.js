7a7df735daa0cf2269635fbb70935bb61f3218aa
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-10-08 17:55:16 +0000

    updated orcid client key

--- clientConfig.js
+++ clientConfig.js
@@ -1,5 +1,5 @@
 const clientConfig = {
-  orcid: { clientId: "APP-IHQJEZNOYXHHNZBM" },
+  orcid: { clientId: "APP-E9FUL2ARI8UYQN6C" },
   linkedin: { clientId: "77e54p8xh4n77e" },
   google: { clientId: "660971614503-m99u7c1ro8u3uen5jft26l5qbnd078d7.apps.googleusercontent.com" },
   facebook: { clientId: "xxxxxxxxxxxxxxxxxx" }
