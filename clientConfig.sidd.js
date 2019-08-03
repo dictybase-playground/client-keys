1694207516b8a3c06c8fd13ced55964e2ecac5bb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2019-08-03 13:35:06 -0500

    add sidd config

---
+++ clientConfig.sidd.js
@@ -0,0 +1,8 @@
+const clientConfig = {
+  orcid: { clientId: "APP-E9FUL2ARI8UYQN6C" },
+  linkedin: { clientId: "77e54p8xh4n77e" },
+  google: { clientId: "4366891928-rv392gfskcqatkpk5u9du8f7ldcntvs3.apps.googleusercontent.com" },
+  facebook: { clientId: "xxxxxxxxxxxxxxxxxx" }
+}
+
+export default clientConfig
